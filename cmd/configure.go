459208803094de95c25bf80f72b4b1b2009d5ec2
CirnoCirnoAndOneMoreCirno <58635649+CirnoCirnoAndOneMoreCirno@users.noreply.github.com>
2022-11-16 23:50:56 +0700

    added configure command

---
+++ cmd/configure.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/spf13/cobra"
+)
+
+var configureCmd = &cobra.Command{
+	Use:   "configure",
+	Short: "",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		keys := []string{"ACCESS_KEY", "SECRET_KEY", "PROJECT_ID", "OUTPUT_FORMAT"}
+		config := make(map[string]string, len(keys))
+		var value string
+		fmt.Println("Available values for OUTPUT_FORMAT: YAML/JSON")
+		for _, key := range keys {
+			fmt.Print(key, "=")
+			_, err := fmt.Scanln(&value)
+			if err != nil {
+				fmt.Println("Error while reading stdin: ", err)
+				return
+			}
+			config[key] = value
+		}
+		err := godotenv.Write(config, ".env")
+		if err != nil {
+			fmt.Println("Error while writing file: ", err)
+			return
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(configureCmd)
+}
