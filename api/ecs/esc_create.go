3c0076299902e0ec1c1ba661fb8fb18115a56537
CirnoCirnoAndOneMoreCirno <58635649+CirnoCirnoAndOneMoreCirno@users.noreply.github.com>
2023-01-26 00:04:47 +0700

    interaction with ecs

--- api/ecs/esc_create.go
+++ api/ecs/esc_create.go
@@ -1,90 +1,98 @@
 package ecs
 
 import (
+	"fmt"
 	"sbercloud-cli/api/models/ecsModels"
+	"sbercloud-cli/internal/handlers/requestMakers"
 )
 
 type escCreateRequest struct {
 	Server ecsCreateParameters `json:"server"`
 }
 
-type ecsCreateParameters struct {
-	AvailabilityZone string `json:"availability_zone"`
-	Name             string `json:"name"`
-	ImageRef         string `json:"imageRef"`
-	RootVolume       struct {
-		Volumetype string `json:"volumetype"`
-	} `json:"root_volume"`
-	DataVolumes []struct {
-		Volumetype    string `json:"volumetype"`
-		Size          int    `json:"size"`
-		Multiattach   bool   `json:"multiattach"`
-		HwPassthrough bool   `json:"hw:passthrough"`
-	} `json:"data_volumes"`
-	FlavorRef      string `json:"flavorRef"`
-	Vpcid          string `json:"vpcid"`
-	SecurityGroups []struct {
-		ID string `json:"id"`
-	} `json:"security_groups"`
-	Nics []struct {
-		SubnetID string `json:"subnet_id"`
-	} `json:"nics"`
-	Publicip struct {
-		Eip struct {
-			Iptype    string `json:"iptype"`
-			Bandwidth struct {
-				Size      int    `json:"size"`
-				Sharetype string `json:"sharetype"`
-			} `json:"bandwidth"`
-		} `json:"eip"`
-	} `json:"publicip"`
-	KeyName    string `json:"key_name"`
-	Count      int    `json:"count"`
-	ServerTags []struct {
-		Key   string `json:"key"`
-		Value string `json:"value"`
-	} `json:"server_tags"`
-	Metadata struct {
-		OpSvcUserid string `json:"op_svc_userid"`
-		AgencyName  string `json:"agency_name"`
-	} `json:"metadata"`
+type dataVolume struct {
+	Volumetype    string `json:"volumetype"`
+	Size          int    `json:"size"`
+	Multiattach   bool   `json:"multiattach"`
+	HwPassthrough bool   `json:"hw:passthrough"`
 }
 
-func CreateECS(projectID, vpcID, imageRef, name, flavorRef, volumeType, subnetID string) (*ecsModels.ESCJobID, error) {
-	/*
-		endpoint := fmt.Sprintf("https://ecs.ru-moscow-1.hc.sbercloud.ru/v1/%s/cloudservers", projectID)
-		ecsRequest := escCreateRequest{Server: ecsCreateParameters{
-			AvailabilityZone: "",
-			Name:             name,
-			ImageRef:         imageRef,
-			RootVolume: struct {
-				Volumetype string `json:"volumetype"`
-			}{},
-			DataVolumes:    nil,
-			FlavorRef:      flavorRef,
-			Vpcid:          vpcID,
-			SecurityGroups: nil,
-			Nics:           nil,
-			Publicip: struct {
-				Eip struct {
-					Iptype    string `json:"iptype"`
-					Bandwidth struct {
-						Size      int    `json:"size"`
-						Sharetype string `json:"sharetype"`
-					} `json:"bandwidth"`
-				} `json:"eip"`
-			}{},
-			KeyName:    "",
-			Count:      0,
-			ServerTags: nil,
-			Metadata: struct {
-				OpSvcUserid string `json:"op_svc_userid"`
-				AgencyName  string `json:"agency_name"`
-			}{},
-		}}
-		var createdJobID ecsModels.ESCJobID
-		err := requestMakers.CreateAndDoRequest(endpoint, requestMakers.HTTP_METHOD_GET, &ecsRequest, &createdJobID, nil)
-		return &createdJobID, err
-	*/
-	return nil, nil
+type secGroupId struct {
+	ID string `json:"id"`
+}
+
+type nic struct {
+	SubnetID string `json:"subnet_id"`
+}
+
+type bandwidth struct {
+	Size      int    `json:"size"`
+	Sharetype string `json:"sharetype"`
+}
+
+type eip struct {
+	Iptype    string    `json:"iptype"`
+	Bandwidth bandwidth `json:"bandwidth"`
+}
+
+type publicIp struct {
+	Eip eip `json:"eip"`
+}
+
+type serverTag struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type rootVolume struct {
+	Volumetype string `json:"volumetype"`
+}
+
+type ecsCreateParameters struct {
+	AvailabilityZone string       `json:"availability_zone"`
+	Name             string       `json:"name"`
+	ImageRef         string       `json:"imageRef"`
+	RootVolume       rootVolume   `json:"root_volume"`
+	DataVolumes      []dataVolume `json:"data_volumes"`
+	FlavorRef        string       `json:"flavorRef"`
+	Vpcid            string       `json:"vpcid"`
+	SecurityGroups   []secGroupId `json:"security_groups"`
+	Nics             []nic        `json:"nics"`
+	AdminPass        string       `json:"adminPass"`
+	//Publicip         publicIp     `json:"publicip"`
+	KeyName    string      `json:"key_name"`
+	Count      int         `json:"count"`
+	ServerTags []serverTag `json:"server_tags"`
+}
+
+func CreateECS(projectID, vpcID, imageRef, name, flavorRef, rootVolumeType string,
+	subnetIds []string, secGroupIds []string, adminPass string, count int) (*ecsModels.ESCJobID, error) {
+
+	endpoint := fmt.Sprintf("https://ecs.ru-moscow-1.hc.sbercloud.ru/v1/%s/cloudservers", projectID)
+	rv := rootVolume{Volumetype: rootVolumeType}
+	subnets := make([]nic, len(subnetIds))
+	for i := 0; i < len(subnetIds); i++ {
+		subnets[i].SubnetID = subnetIds[i]
+	}
+	secGroups := make([]secGroupId, len(secGroupIds))
+	for i := 0; i < len(secGroupIds); i++ {
+		secGroups[i].ID = secGroupIds[i]
+	}
+	ecsRequest := escCreateRequest{Server: ecsCreateParameters{
+		AvailabilityZone: "",
+		Name:             name,
+		ImageRef:         imageRef,
+		RootVolume:       rv,
+		FlavorRef:        flavorRef,
+		Vpcid:            vpcID,
+		SecurityGroups:   secGroups,
+		Nics:             subnets,
+		KeyName:          "",
+		AdminPass:        adminPass,
+		Count:            count,
+		ServerTags:       nil,
+	}}
+	var createdJobID ecsModels.ESCJobID
+	err := requestMakers.CreateAndDoRequest(endpoint, requestMakers.HTTP_METHOD_POST, &ecsRequest, &createdJobID, nil)
+	return &createdJobID, err
 }
--- cmd/ecs.go
+++ cmd/ecs.go
@@ -60,15 +60,39 @@ var ecsInfoCmd = &cobra.Command{
 	},
 }
 
-var ecsGetDeleteIds []string
-var deletePublicIp bool
-var deleteVolume bool
+var ecsDeleteIds []string
+var ecsDeletePublicIp bool
+var ecsDeleteVolume bool
 var ecsDeleteCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "Delete ECS",
 	Long:  `Delete ECS`,
 	Run: func(cmd *cobra.Command, args []string) {
-		ecs, err := ecs.DeleteEcs(ProjectID, ecsGetDeleteIds, deletePublicIp, deleteVolume)
+		ecs, err := ecs.DeleteEcs(ProjectID, ecsDeleteIds, ecsDeletePublicIp, ecsDeleteVolume)
+		if err != nil {
+			beautyfulPrints.PrintError(err)
+		} else {
+			beautyfulPrints.PrintStruct(ecs, jmesPathQuery)
+		}
+	},
+}
+
+var ecsCreateVpcID string
+var ecsCreateImageRef string
+var ecsCreateName string
+var ecsCreateFlavorRef string
+var ecsCreateRootVolumeType string
+var ecsCreateSubnetIds []string
+var ecsCreateSecGroupIds []string
+var ecsCreateAdminPass string
+var ecsCreateCount int
+var ecsCreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Delete ECS",
+	Long:  `Delete ECS`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ecs, err := ecs.CreateECS(ProjectID, ecsCreateVpcID, ecsCreateImageRef, ecsCreateName, ecsCreateFlavorRef,
+			ecsCreateRootVolumeType, ecsCreateSubnetIds, ecsCreateSecGroupIds, ecsCreateAdminPass, ecsCreateCount)
 		if err != nil {
 			beautyfulPrints.PrintError(err)
 		} else {
@@ -85,12 +109,23 @@ func init() {
 	ecsCmd.AddCommand(ecsListCmd)
 	ecsCmd.AddCommand(ecsInfoCmd)
 	ecsCmd.AddCommand(ecsDeleteCmd)
+	ecsCmd.AddCommand(ecsCreateCmd)
 
 	ecsFlavorListCmd.Flags().StringVarP(&ecsFlavorListAvailabilityZone, "availability_zone", "a", "", "")
 
 	ecsInfoCmd.Flags().StringVarP(&ecsGetInfoId, "id", "i", "", "")
 
-	ecsDeleteCmd.Flags().StringSliceVarP(&ecsGetDeleteIds, "id", "i", nil, "")
-	ecsDeleteCmd.Flags().BoolVar(&deletePublicIp, "del-ip", false, "")
-	ecsDeleteCmd.Flags().BoolVar(&deleteVolume, "del-vol", false, "")
+	ecsDeleteCmd.Flags().StringSliceVarP(&ecsDeleteIds, "id", "i", nil, "")
+	ecsDeleteCmd.Flags().BoolVar(&ecsDeletePublicIp, "del-ip", false, "")
+	ecsDeleteCmd.Flags().BoolVar(&ecsDeleteVolume, "del-vol", false, "")
+
+	ecsCreateCmd.Flags().StringVar(&ecsCreateVpcID, "vpc-id", "", "")
+	ecsCreateCmd.Flags().StringVar(&ecsCreateImageRef, "image-ref", "", "")
+	ecsCreateCmd.Flags().StringVar(&ecsCreateName, "name", "", "")
+	ecsCreateCmd.Flags().StringVar(&ecsCreateFlavorRef, "flavor-ref", "", "")
+	ecsCreateCmd.Flags().StringVar(&ecsCreateRootVolumeType, "root-volume-type", "", "")
+	ecsCreateCmd.Flags().StringSliceVar(&ecsCreateSubnetIds, "subnet-ids", nil, "")
+	ecsCreateCmd.Flags().StringSliceVar(&ecsCreateSecGroupIds, "sg-ids", nil, "")
+	ecsCreateCmd.Flags().StringVar(&ecsCreateAdminPass, "admin-pass", "", "")
+	ecsCreateCmd.Flags().IntVar(&ecsCreateCount, "count", 1, "")
 }
